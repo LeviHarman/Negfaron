ec4e39684e2f1ee4259f72f061c07e83e49ca044
Levi Harman <serpentinexcubed@gmail.com>
2012-04-26 07:26:05 -0500

    removed some files

--- files/weapon.rb
+++
@@ -1,9 +0,0 @@
-class Weapon
-  attr_accessor :name, :hitdie, :number_rolls, :price
-  def initialize(name, hitdie, number_rolls, price)
-    @name = name
-    @hitdie = hitdie
-    @number_rolls = number_rolls
-    @price = price
-  end
-end
